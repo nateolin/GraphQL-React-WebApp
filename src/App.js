69d6c3fc6681817586b0f63d23f25d6aac83db85
Savanna Olin <savannaolin@savannas-mbp.lan>
2020-04-21 21:07:11 -0500

    Adding Nav Bar and style sheet for nav bar

--- src/App.js
+++ src/App.js
@@ -5,16 +5,22 @@ import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import AuthPage from "./pages/Auth";
 import EventsPage from "./pages/Events";
 import BookingsPage from "./pages/Bookings";
+import MainNavigation from "./components/navigation/MainNavigation";
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Redirect from="/" to="/auth" exact />
-        <Route path="/auth" component={AuthPage} />
-        <Route path="/events" component={EventsPage} />
-        <Route path="/bookings" component={BookingsPage} />
-      </Switch>
+      <React.Fragment>
+        <MainNavigation />
+        <main>
+          <Switch>
+            <Redirect from="/" to="/auth" exact />
+            <Route path="/auth" component={AuthPage} />
+            <Route path="/events" component={EventsPage} />
+            <Route path="/bookings" component={BookingsPage} />
+          </Switch>
+        </main>
+      </React.Fragment>
     </BrowserRouter>
   );
 }
