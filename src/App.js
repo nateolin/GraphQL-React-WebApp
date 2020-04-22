5570533f1efc1630fe6f45f9121081826b84ef40
Savanna Olin <savannaolin@savannas-mbp.lan>
2020-04-21 20:11:45 -0500

    Finishing touches for initial navigation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
 import AuthPage from "./pages/Auth";
+import EventsPage from "./pages/Events";
+import BookingsPage from "./pages/Bookings";
 
 function App() {
   return (
@@ -10,8 +12,8 @@ function App() {
       <Switch>
         <Redirect from="/" to="/auth" exact />
         <Route path="/auth" component={AuthPage} />
-        <Route path="/events" component={null} />
-        <Route path="/bookings" component={null} />
+        <Route path="/events" component={EventsPage} />
+        <Route path="/bookings" component={BookingsPage} />
       </Switch>
     </BrowserRouter>
   );
