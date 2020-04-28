3e56a14c189b942e758c944350e29d035aafa355
Savanna Olin <savannaolin@savannas-mbp.lan>
2020-04-28 17:01:25 -0500

    WIP - setting up API communication

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,24 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './Auth.css';
+import "./Auth.css";
 
 class AuthPage extends Component {
-    render() {
-        return <form className="auth-form">
-            <div className="form-control">
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email"></input>
-            </div>
-            <div className="form-control">
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password"></input>
-            </div>
-            <div className="form-actions">
-                <button type="button">Switch to Signup</button>
-                <button type="submit">Submit</button>
-            </div>
-        </form>
+    constructor(props) {
+        super(props);
+        this.emailEl = React.createRef();
+        this.passwordEl = React.createRef();
     }
+    
+    submitHandler = (event) => {
+        event.preventDefault();
+        const email = this.emailEl.current.value;
+        const password = this.passwordEl.current.value;
+
+        if (email.trim().length === 0 || password.trim().length === 0 ) {
+            return;
+        }
+
+        const requestBody = {};
+
+        fetch('http://localhost:8000/graphql', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    };
+
+  render() {
+    return (
+      <form className="auth-form" onSubmit={this.submitHandler}>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" ref={this.emailEl}></input>
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" ref={this.passwordEl}></input>
+        </div>
+        <div className="form-actions">
+          <button type="submit">Submit</button>
+          <button type="button">Switch to Signup</button>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default AuthPage;
